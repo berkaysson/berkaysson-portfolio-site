1ed77b8a207f0e93e865f9bb2778b545cee2f407
berkaysson <berkaysonel85@gmail.com>
2023-07-06 20:56:10 +0300

    Add Skills component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import About from './Components/About';
 import Contact from './Components/Contact';
 import Navigation from './Components/Navigation';
 import Projects from './Components/Projects';
+import Skills from './Components/Skills';
 
 const App = () => {
   return (
     <div className="App">
       <Navigation />
       <About />
+      <Skills />
       <Projects />
       <Contact />
     </div>
