805ab22f5e195cad7ee1e36061c84907c8a07d38
berkaysson <berkaysonel85@gmail.com>
2023-07-06 19:46:30 +0300

    Create main components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Navigation from './Components/Navigation';
+import Projects from './Components/Projects';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <About />
+      <Projects />
+      <Contact />
     </div>
   );
 }
