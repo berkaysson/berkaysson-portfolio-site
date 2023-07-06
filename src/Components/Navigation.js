863278b16fbbc40f41731d4ae4d3efb8640a37ce
berkaysson <berkaysonel85@gmail.com>
2023-07-06 20:03:24 +0300

    Add main navigation content

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,5 +1,12 @@
 const Navigation = () => {
-  return 
+  return <div>
+    <a href="/">Logo</a>
+    <div>
+      <a href="/">About</a>
+      <a href="/">Projects</a>
+      <a href="/">Contact</a>
+    </div>
+  </div>
 }
 
 export default Navigation;
