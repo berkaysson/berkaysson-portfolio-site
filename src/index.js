1f5251389b810e8ba93929840a7efdaca4b24299
berkaysson <berkaysonel85@gmail.com>
2023-07-12 21:28:57 +0300

    Add ThemeProvider from styled-components

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Styles/index.css';
 import App from './App';
+import { ThemeProvider } from "styled-components";
+import { theme } from './Styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
