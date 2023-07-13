9d500d50ff101b47a219fe8cb1f8a77de729cb83
berkaysson <berkaysonel85@gmail.com>
2023-07-13 15:48:37 +0300

    Logo styles

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,16 +3,30 @@ import styled from "styled-components";
 const NavigationWrapper = styled.nav`
   width: 100px;
   height: 100%;
-  position: absolute;
+  /* position: absolute; */
   border: 1px solid red;
   z-index: 3;
   min-height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .logo{
+    display: block;
+    padding: 8px 0;
+    img{
+      display: block;
+      margin: 8px auto;
+      width: 25px;
+      height: auto;
+    }
+  }
 `;
 
 const Navigation = () => {
   return (
     <NavigationWrapper>
-      <a href="/">
+      <a href="/" className="logo">
         <img src="" alt="avatar" /> <span>berkaysson</span>
       </a>
       <a href="/">About</a>
