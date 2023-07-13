15592917d692ba6f30b514b55618a044e9828630
berkaysson <berkaysonel85@gmail.com>
2023-07-13 16:50:37 +0300

    Main style for Navigation menu

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-const NavigationWrapper = styled.nav`
+const NavigationWrapper = styled.div`
   width: 100px;
   height: 100%;
-  /* position: absolute; */
+  position: fixed;
   border: 1px solid red;
   z-index: 3;
   min-height: 500px;
@@ -11,15 +11,33 @@ const NavigationWrapper = styled.nav`
   flex-direction: column;
   align-items: center;
 
-  .logo{
+  .logo {
     display: block;
     padding: 8px 0;
-    img{
+    img {
       display: block;
       margin: 8px auto;
       width: 25px;
       height: auto;
     }
+  }
+`;
+
+const NavigationMenuWrapper = styled.nav`
+  display: block;
+  text-align: center;
+  position: absolute;
+  height: 220px;
+  top: 50%;
+  width: 100%;
+  margin-top: -120px;
+
+  a{
+    font-size: 22px;
+    display: block;
+    line-height: 50px;
+    height: 50px;
+    position: relative;
   }
 `;
 
@@ -29,9 +47,11 @@ const Navigation = () => {
       <a href="/" className="logo">
         <img src="" alt="avatar" /> <span>berkaysson</span>
       </a>
-      <a href="/">About</a>
-      <a href="/">Projects</a>
-      <a href="/">Contact</a>
+      <NavigationMenuWrapper>
+        <a href="/">About</a>
+        <a href="/">Projects</a>
+        <a href="/">Contact</a>
+      </NavigationMenuWrapper>
     </NavigationWrapper>
   );
 };
