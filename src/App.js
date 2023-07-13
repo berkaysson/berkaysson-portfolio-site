081e13357f653316ed1c983a4500548b9d6d3c32
berkaysson <berkaysonel85@gmail.com>
2023-07-13 16:50:54 +0300

    Add MainWrapper to wrap contents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import About from './Components/About';
-import Contact from './Components/Contact';
-import Navigation from './Components/Navigation';
-import Projects from './Components/Projects';
-import Skills from './Components/Skills';
-import styled  from 'styled-components';
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Navigation from "./Components/Navigation";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+import styled from "styled-components";
 
-const AppWrapper = styled.main`
+const AppWrapper = styled.div``;
 
+const MainWrapper = styled.main`
+  margin-left: 100px;
 `;
 
 const App = () => {
   return (
     <AppWrapper>
       <Navigation />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+      <MainWrapper>
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </MainWrapper>
     </AppWrapper>
   );
-}
+};
 
 export default App;
