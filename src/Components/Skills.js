f092a64aad0e169bd7dab39f97deee2160bc6ed8
berkaysson <berkaysonel85@gmail.com>
2023-07-14 17:27:50 +0300

    Use SkillItem and create styles of Skills section

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,18 +1,51 @@
+import { styled } from "styled-components";
+import SkillItem from "./SkillItem";
+
+const SkillsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  border: 1px solid red;
+  padding: ${({ theme }) => theme.sizes.xlg};
+
+  h2 {
+    font-size: ${({ theme }) => theme.sizes.lg};
+  }
+`;
+
+const SkillsListWrapper = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-top: ${({ theme }) => theme.sizes.lg};
+  gap: ${({ theme }) => theme.sizes.xsl};
+`;
+
 const Skills = () => {
-  return <div>
-    <h2>What are My Skills</h2>
-    <ul>
-      <li>JavaScript</li>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>React.js</li>
-      <li>React Router</li>
-      <li>Material UI</li>
-      <li>npm</li>
-      <li>GIT</li>
-      <li>Styled-Components</li>
-    </ul>
-  </div>
-}
+  const skills = [
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React.js",
+    "React Router",
+    "Material UI",
+    "npm",
+    "git",
+    "Styled-Components",
+  ];
+  return (
+    <SkillsWrapper>
+      <h2>What are My Skills</h2>
+      <SkillsListWrapper>
+        {
+          skills.map((skill)=>(<SkillItem skill={skill} />))
+        }
+      </SkillsListWrapper>
+    </SkillsWrapper>
+  );
+};
 
 export default Skills;
