9dc3f7f70f1cd8bb603b6c59e97119f325421dd9
berkaysson <berkaysonel85@gmail.com>
2023-07-16 23:39:45 +0300

    Create main content for contact section

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,29 @@
 const Contact = () => {
   return <div>
-    Contact Form
+    <h1>Say Hi</h1>
+    <div>
+      <p>My links</p>
+      <a href="/">Github</a>
+      <a href="/">Email</a>
+      <a href="/">Spotify</a>
+      <a href="/">dev.com</a>
+    </div>
+
+    <form>
+      <label>
+        Your name
+        <input type="text" />
+      </label>
+      <label>
+        Your email
+        <input type="email" />
+      </label>
+      <label>
+        Your Message
+        <input type="text" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   </div>
 }
 
