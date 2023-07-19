c6d30e2df807bf4f5b249f403a1975cbc6ee0561
berkaysson <berkaysonel85@gmail.com>
2023-07-19 16:07:08 +0300

    Fix to scrolling top beforre loading new page

--- src/Hooks/ScrollToTop.js
+++ src/Hooks/ScrollToTop.js
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   // Extracts pathname property(key) from an object
@@ -7,8 +7,11 @@ const ScrollToTop = () => {
 
   // Automatically scrolls to top whenever pathname changes
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // it should be equal to duration time of page transitions
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 500);
   }, [pathname]);
-}
+};
 
 export default ScrollToTop;
