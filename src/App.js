55dbd6db701ae3b897180f290659a55e84367ea3
berkaysson <berkaysonel85@gmail.com>
2023-07-22 00:32:37 +0300

    Implement MouseFollower

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AnimatedRoutes from "./Components/AnimatedRoutes";
 import Navigation from "./Components/Navigation";
 import useMousePosition from "./Hooks/useMousePosition.js";
 import MouseTooltip from "./Hooks/MouseToolTip";
+import MouseFollower from "./Components/MouseFollower";
 
 const AppWrapper = styled.div`
   color: ${({ theme }) => theme.colors.primary};
@@ -22,38 +23,6 @@ const MainWrapper = styled.main`
   padding-top: 102px;
 `;
 
-const MouseFollower = styled.div`
-  width: 62px;
-  height: 62px;
-  background-color: transparent;
-  backdrop-filter: blur(10px) contrast(1.5);
-  border-radius: 50%;
-  pointer-events: none;
-  font-size: 12px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #abc4ff;
-  text-shadow: 0 0 1px black;
-  transition: opacity 0.2s, transform 0.8s;
-  opacity: ${({ visible }) => (visible ? 1 : 0)};
-  transform: ${({ visible }) =>
-    visible
-      ? "scale(1) translate(-50%, -70%)"
-      : "scale(0.5) translate(-50%, -70%)"};
-
-  &::before {
-    position: absolute;
-    content: "";
-    background-color: black;
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    opacity: 0.6;
-    z-index: -1;
-  }
-`;
-
 const App = () => {
   const { target: onMouseTarget } = useMousePosition();
   return (
@@ -61,11 +30,9 @@ const App = () => {
       <MouseTooltip
         children={
           <MouseFollower
+            text={"PREVIEW"}
             visible={onMouseTarget === "lifemapImg"}
-            onMouseTarget={onMouseTarget}
-          >
-            PREVIEW
-          </MouseFollower>
+          />
         }
       />
 
