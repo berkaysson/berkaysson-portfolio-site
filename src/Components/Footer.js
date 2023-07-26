88f5fc4da6374c88a36aaa1f5eb104ab96dfa862
berkaysson <berkaysonel85@gmail.com>
2023-07-26 23:32:48 +0300

    Add main styles for footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,48 @@
+import { styled } from "styled-components";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const FooterWrapper = styled.footer`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  align-items: center;
+  border: 1px solid red;
+  padding: ${({ theme }) => theme.sizes.sl};
+  font-size: 14px;
+`;
+
+const AnchorsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+`;
+
+const AnchorWrapper = styled.a`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.4rem;
+
+  &:hover{
+    text-decoration: underline;
+  }
+`;
+
 const Footer = () => {
-  return <div>
-    Footer
-  </div>
-}
+  return (
+    <FooterWrapper>
+      <span>©Berkay Sonel - 2023</span>
+      <AnchorsContainer>
+        <AnchorWrapper href="/">
+          <GitHubIcon /> <span>Github</span>
+        </AnchorWrapper>
+        <AnchorWrapper href="/">
+          <EmailIcon /> <span>Email</span>
+        </AnchorWrapper>
+      </AnchorsContainer>
+    </FooterWrapper>
+  );
+};
 
 export default Footer;
