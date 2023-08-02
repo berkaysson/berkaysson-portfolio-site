29018c55046735e61cd2dfa26574cbef5dd011e6
berkaysson <berkaysonel85@gmail.com>
2023-08-02 20:06:54 +0300

    Add useInView hook

--- src/Components/Projects/ProjectContentItem.js
+++ src/Components/Projects/ProjectContentItem.js
@@ -1,3 +1,5 @@
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 import { styled } from "styled-components";
 
 const ProjectContentItemWrapper = styled.div`
@@ -9,6 +11,11 @@ const ProjectContentItemWrapper = styled.div`
   padding: 2rem;
   grid-gap: 1rem;
   justify-content: center;
+  transition: ${({ theme, inView }) =>
+    inView ? theme.transitions.main : "none"};
+  transform: scale(${({ inView }) => (inView ? 1 : 0)});
+  opacity: ${({ inView }) => (inView ? 1 : 0)};
+  filter: blur(${({ inView }) => (inView ? 0 : "3px")});
 
   h2 {
     grid-area: 1/1/2/3;
@@ -26,8 +33,11 @@ const ProjectContentItemWrapper = styled.div`
 `;
 
 const ProjectContentItem = ({ order, children }) => {
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true });
+
   return (
-    <ProjectContentItemWrapper order={order}>
+    <ProjectContentItemWrapper order={order} ref={ref} inView={inView}>
       {children}
     </ProjectContentItemWrapper>
   );
