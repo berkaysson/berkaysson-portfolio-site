9cd9e969d2daa7474ffd14124c80cd664abbfc5e
berkaysson <berkaysonel85@gmail.com>
2023-08-06 06:48:40 +0300

    Add glassy style to navigation bar

--- src/Components/Navigation/NavigationStyles.js
+++ src/Components/Navigation/NavigationStyles.js
@@ -2,16 +2,23 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 export const NavigationWrapper = styled(motion.div)`
-  width: 100%;
   height: 100px;
   position: fixed;
   top: 0;
-  border: 1px solid red;
   z-index: 3;
   display: flex;
   align-items: center;
   justify-content: space-between;
   opacity: 1;
+  margin: ${({ theme }) => theme.sizes.md} 0;
+
+  //glassmorphism
+  background: rgba(255, 255, 255, 0.3);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.6);
 
   .logo {
     padding: 0 ${({ theme }) => theme.sizes.sl};
@@ -32,12 +39,11 @@ export const NavigationMenuWrapper = styled.nav`
   flex-direction: row;
   align-items: center;
   gap: ${({ theme }) => theme.sizes.xlg};
-  margin-right: ${({ theme }) => theme.sizes.xlg};
-  font-size: ${({ theme }) => theme.sizes.lg};
+  margin: 0 ${({ theme }) => theme.sizes.xlg};
+  font-size: ${({ theme }) => theme.sizes.md};
 
   @media (max-width: 500px) {
     gap: ${({ theme }) => theme.sizes.md};
-    font-size: ${({ theme }) => theme.sizes.md};
     margin-right: ${({ theme }) => theme.sizes.sl};
   }
 `;
