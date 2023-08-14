80c81357f059e53085451a3c3cdab88fc78eb25e
berkaysson <berkaysonel85@gmail.com>
2023-08-14 21:48:54 +0300

    Replace backdrop filter with regular filter

--- src/Components/Wrappers/Effects/GlassCardWrapper.js
+++ src/Components/Wrappers/Effects/GlassCardWrapper.js
@@ -3,8 +3,7 @@ import { styled } from "styled-components";
 export const GlassCardWrapper = styled.div`
   background: rgba(255, 255, 255, 0.7);
   border-radius: 16px;
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.2);
-  backdrop-filter: blur(5px);
-  -webkit-backdrop-filter: blur(5px);
+  box-shadow: 0 8px 10px rgba(0, 0, 0, 0.2);
   border: 1px solid rgba(255, 255, 255, 0.6);
+  filter: drop-shadow(0px 0px 3px rgba(150, 150, 150, 0.8));
 `;
