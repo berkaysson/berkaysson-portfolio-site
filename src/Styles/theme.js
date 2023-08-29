3c4de966162f42a5bfe87e030ed428354fdb501d
berkaysson <berkaysonel85@gmail.com>
2023-08-29 23:25:12 +0300

    Refactor the color palette and implement it to other properties

--- src/Styles/theme.js
+++ src/Styles/theme.js
@@ -1,10 +1,20 @@
+const COLORS = {
+  darker: "#141A26",
+  dark: "#242D40",
+  medium: "#3C5E73",
+  light: "#547C8C",
+  lighter: "#8D9FA6",
+  lightest: "#BEE5F2",
+};
+
 export const theme = {
   colors: {
-    primary: "#000000",
-    secondary: "##EBFDFF",
-    alternative: "#97CAEF",
-    theme: "#FC4445",
-    themeSecondary: "#3FEEE6",
+    darker: COLORS.darker,
+    dark: COLORS.dark,
+    medium: COLORS.medium,
+    light: COLORS.light,
+    lighter: COLORS.lighter,
+    lightest: COLORS.lightest,
   },
   sizes: {
     xsl: "8px",
@@ -17,16 +27,16 @@ export const theme = {
     main: "filter 0.2s ease, transform 0.2s ease, opacity 0.2s ease",
   },
   boxShadows: {
-    neumorphism:{
-      convex:{
-        boxShadow:  "4px 4px 5px #d6d6d6,-4px -4px 5px #ffffff",
-        background: "linear-gradient(145deg, #ffffff, #e6e6e6)",
+    neumorphism: {
+      convex: {
+        boxShadow: `4px 4px 5px ${COLORS.medium},-4px -4px 5px ${COLORS.dark}`,
+        background: `linear-gradient(145deg, ${COLORS.dark}, ${COLORS.medium})`,
       },
-      concave:{
-        boxShadow:  "4px 4px 5px #d6d6d6,-4px -4px 5px #ffffff",
-        background: "linear-gradient(145deg, #e6e6e6, #ffffff)",
-      }
-    }
+      concave: {
+        boxShadow: `4px 4px 5px ${COLORS.medium},-4px -4px 5px ${COLORS.dark}`,
+        background: `linear-gradient(145deg, ${COLORS.medium}, ${COLORS.dark})`,
+      },
+    },
   },
   framerAnimations: {
     pageTransition: {
@@ -34,19 +44,19 @@ export const theme = {
         opacity: 0,
         y: -100,
         transform: "scale(0.7)",
-        filter: "blur(6px)"
+        filter: "blur(6px)",
       },
       animate: {
         opacity: 1,
         y: 0,
         transform: "scale(1)",
-        filter: "blur(0)"
+        filter: "blur(0)",
       },
       exit: {
         opacity: 0,
         y: -100,
         transform: "scale(0.7)",
-        filter: "blur(6px)"
+        filter: "blur(6px)",
       },
       transition: {
         duration: 0.7,
@@ -56,15 +66,15 @@ export const theme = {
     navTransition: {
       visible: {
         opacity: 1,
-        y:0
+        y: 0,
       },
       hidden: {
         opacity: 0,
-        y:-50,
+        y: -50,
       },
       exit: {
         opacity: 0,
-        y:-50
+        y: -50,
       },
       transition: { ease: [0.5, 1, 0.5, 1], duration: 0.5 },
     },
