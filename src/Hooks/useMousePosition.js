a001a856ccfe114797178160f22cc499d56ff7d0
berkaysson <berkaysonel85@gmail.com>
2023-09-05 11:35:45 +0300

    Fix not updating target when scroll event triggers

--- src/Hooks/useMousePosition.js
+++ src/Hooks/useMousePosition.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+
 const useMousePosition = () => {
   const [mousePosition, setMousePosition] = useState({
     x: null,
@@ -6,20 +7,43 @@ const useMousePosition = () => {
     targetID: null,
     target: null,
   });
+
   useEffect(() => {
     const updateMousePosition = (ev) => {
       setMousePosition({
         x: ev.clientX,
         y: ev.clientY,
         targetID: ev.target.id,
-        target: ev.target
+        target: ev.target,
       });
     };
+
+    const updateMousePositionOnScroll = () => {
+      if (mousePosition.x !== null && mousePosition.y !== null) {
+        setMousePosition((prevMousePosition) => ({
+          ...prevMousePosition,
+          targetID: document.elementFromPoint(
+            prevMousePosition.x,
+            prevMousePosition.y
+          ).id,
+          target: document.elementFromPoint(
+            prevMousePosition.x,
+            prevMousePosition.y
+          ),
+        }));
+      }
+    };
+
     window.addEventListener("mousemove", updateMousePosition);
+    window.addEventListener("scroll", updateMousePositionOnScroll);
+
     return () => {
       window.removeEventListener("mousemove", updateMousePosition);
+      window.removeEventListener("scroll", updateMousePositionOnScroll);
     };
-  }, []);
+  }, [mousePosition]);
+
   return mousePosition;
 };
+
 export default useMousePosition;
