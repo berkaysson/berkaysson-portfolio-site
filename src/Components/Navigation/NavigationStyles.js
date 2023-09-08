83290dc3a2ba94aa04ab0cf3df92e18568d92cc3
berkaysson <berkaysonel85@gmail.com>
2023-09-08 23:08:00 +0300

    Responsive design for navigation

--- src/Components/Navigation/NavigationStyles.js
+++ src/Components/Navigation/NavigationStyles.js
@@ -7,12 +7,27 @@ export const NavigationWrapper = styled.div`
   justify-content: center;
   opacity: 1;
   color: ${({ theme }) => theme.colors.darker};
+  padding: 0 2rem;
 
   .logo {
     svg {
       margin: auto ${({ theme }) => theme.sizes.md};
       width: 90px;
+
+      @media (max-width: 475px) {
+      }
     }
+  }
+
+  @media (max-width: 475px) {
+    justify-content: space-between;
+  }
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+    padding: 1rem;
+    height: auto;
+    gap: 1.5rem;
   }
 `;
 
@@ -20,12 +35,17 @@ export const NavigationMenuWrapper = styled.nav`
   display: flex;
   flex-direction: row;
   align-items: center;
-  gap: ${({ theme }) => theme.sizes.xlg};
-  margin: 0 ${({ theme }) => theme.sizes.xlg};
   font-size: ${({ theme }) => theme.sizes.md};
+  margin-left: 4rem;
+  gap: 0.5rem;
 
-  @media (max-width: 500px) {
-    gap: ${({ theme }) => theme.sizes.md};
-    margin-right: ${({ theme }) => theme.sizes.sl};
+  @media (max-width: 475px) {
+    margin-left: 1rem;
+  }
+
+  @media (max-width: 425px) {
+    padding: 0 2rem;
+    margin-left: 0;
+    gap: 2rem;
   }
 `;
