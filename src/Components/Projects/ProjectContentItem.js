3037976458f0fe72e723d5df8494cdd59d9401fb
berkaysson <berkaysonel85@gmail.com>
2023-09-19 17:56:29 +0300

    Refactor layout of ProjectContentItemWrapper

--- src/Components/Projects/ProjectContentItem.js
+++ src/Components/Projects/ProjectContentItem.js
@@ -4,17 +4,19 @@ import { styled } from "styled-components";
 
 const ProjectContentItemWrapper = styled.div`
   display: grid;
-  grid-template-columns: 50% 50%;
-  grid-template-rows: auto auto;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto auto auto;
   position: relative;
   grid-gap: 1rem;
   justify-content: center;
+  justify-items: center;
   transition: ${({ theme, inView }) =>
     inView ? theme.transitions.main : "none"};
   transform: scale(${({ inView }) => (inView ? 1 : 0)});
   opacity: ${({ inView }) => (inView ? 1 : 0)};
   filter: blur(${({ inView }) => (inView ? 0 : "3px")});
   background-color: ${({ theme }) => theme.colors.darker};
+  max-width: 1300px;
 
   &:last-child {
     margin-bottom: 1rem;
@@ -24,22 +26,24 @@ const ProjectContentItemWrapper = styled.div`
   }
 
   h2 {
-    grid-area: 1/1/2/3;
     font-size: 2rem;
   }
 
   p,
   ul {
-    grid-area: ${({ order }) => (order % 2 === 0 ? "2/1/3/2" : "2/2/3/3")};
+    width: 70%;
+
+    @media (max-width: 1024px) {
+      width: 100%;
+    }
   }
 
   img {
-    grid-area: ${({ order }) => (order % 2 === 0 ? "2/2/3/2" : "2/1/3/2")};
+    width: 100%;
   }
 
   hr {
     margin: 0 auto;
-    grid-column: 1/3;
     height: 1px;
     width: 20%;
     border-color: ${({ theme }) => theme.colors.dark};
@@ -51,14 +55,6 @@ const ProjectContentItemWrapper = styled.div`
 
   @media (max-width: 1024px) {
     grid-template-rows: auto auto auto;
-    p,
-    ul {
-      grid-area: 3/1/4/3;
-    }
-
-    img {
-      grid-area: 2/1/3/3;
-    }
   }
 `;
 
