abc420e9c3438bb1e1b65f3f4c7c56133a8776d7
berkaysson <berkaysonel85@gmail.com>
2023-09-19 21:26:27 +0300

    Create skillsData

--- src/Components/Sections/Skills/Skills.js
+++ src/Components/Sections/Skills/Skills.js
@@ -1,5 +1,6 @@
 import { styled } from "styled-components";
 import SkillItem from "./SkillItem";
+import { skillsData } from "./skillsData";
 
 const SkillsWrapper = styled.div`
   display: flex;
@@ -39,45 +40,11 @@ const SkillsListWrapper = styled.ul`
 `;
 
 const Skills = () => {
-  const skills = [
-    {
-      name: "JavaScript",
-      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-    },
-    {
-      name: "HTML",
-      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
-    },
-    {
-      name: "CSS",
-      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
-    },
-    {
-      name: "React.js",
-      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-    },
-    {
-      name: "Material UI",
-      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-plain.svg",
-    },
-    {
-      name: "Node.js",
-      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg",
-    },
-    {
-      name: "git",
-      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
-    },
-    {
-      name: "styled-components",
-      img: "https://avatars.githubusercontent.com/u/20658825?s=200&v=4",
-    },
-  ];
   return (
     <SkillsWrapper id="skills">
       <h2>What are my Skills ?</h2>
       <SkillsListWrapper>
-        {skills.map((skill, index) => (
+        {skillsData.map((skill, index) => (
           <SkillItem skill={skill} key={index} index={index} />
         ))}
       </SkillsListWrapper>
---
+++ src/Components/Sections/Skills/skillsData.js
@@ -0,0 +1,34 @@
+export const skillsData = [
+  {
+    name: "JavaScript",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "HTML",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
+  },
+  {
+    name: "CSS",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+  },
+  {
+    name: "React.js",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  },
+  {
+    name: "Material UI",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-plain.svg",
+  },
+  {
+    name: "Node.js",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg",
+  },
+  {
+    name: "git",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
+  },
+  {
+    name: "styled-components",
+    img: "https://avatars.githubusercontent.com/u/20658825?s=200&v=4",
+  },
+];
