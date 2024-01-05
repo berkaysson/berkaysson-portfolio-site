1990cf2383fe0b5cd700e9be34d6eecfac6e6901
berkaysson <berkaysonel85@gmail.com>
2024-01-05 20:26:35 +0300

    Refactor content

--- src/Components/Sections/About.js
+++ src/Components/Sections/About.js
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import BerkayAvatar from "../../Images/berkay-avatar.png";
+import BerkayAvatar from "../../Images/berkay-bg2.png";
 import AvatarImage from "../Wrappers/Effects/AvatarImage";
 import ContactButton from "../Wrappers/Effects/ContactButton";
 
@@ -52,6 +52,7 @@ const AboutSection = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
   width: 100%;
   gap: ${({ theme }) => theme.sizes.md};
   padding: ${({ theme }) => theme.sectionPadding};
@@ -61,8 +62,7 @@ const AboutSection = styled.section`
   }
 
   p {
-    /* font-size: ${({ theme }) => theme.sizes.sl}; */
-    max-width: 1300px;
+    max-width: 1030px;
 
     &:last-child {
       padding-top: 1rem;
@@ -83,9 +83,7 @@ const About = () => {
       <AboutHeader>
         <div>
           <h1>Hi, I'm Berkay Sonel</h1>
-          <h3>
-            I am a front-end developer. I love creating awesome web experiences.
-          </h3>
+          <h3>creating awesome web experiences</h3>
         </div>
         <AvatarImage src={BerkayAvatar} alt="berkayPhoto" />
       </AboutHeader>
@@ -96,13 +94,7 @@ const About = () => {
             From a young age, I've always been drawn to problem-solving and
             bringing ideas to life, which is why I initially pursued a career in
             Civil Engineering. However, I soon realized that coding was my true
-            ambition and made the decision to switch paths. One significant
-            moment that solidified my passion for coding and made me realize I
-            couldn't find the same joy in Civil Engineering was when I was 14
-            and started modding the game Mount and Blade using Python. It was an
-            eye-opening experience that allowed me to explore the power of
-            coding, customize the game to my liking, create things, and witness
-            the endless possibilities of digital creation.
+            ambition and made the decision to switch paths.
           </p>
           <br />
           <p>
@@ -110,12 +102,15 @@ const About = () => {
             continuous learning. The Odin Project, an invaluable resource, has
             played a pivotal role in shaping my expertise in modern web
             development practices. Through resources like The Odin Project, I've
-            sharpened my skills in modern web development practices. I have
-            learned and developed my skills in JavaScript, HTML, CSS, React.js,
-            and various other technologies. With these skills, I have the
-            ability to create web applications and develop user interfaces. I
-            continuously expand my skill set and stay up-to-date with the latest
-            industry trends
+            sharpened my skills in modern web development practices. Then, I
+            have been accepted into an educational program organized by
+            Microsoft Turkey and Koç Holding, Ctrl + Future. Out of 12000
+            applicants, only 400 of us were selected for this opportunity. After
+            studying hard and preparing well, I was able to pass the exams and
+            interviews for the internship program at KoçSistem. It was a great
+            opportunity to learn from experienced mentors and work on
+            challenging projects. I gained a lot of valuable skills and
+            knowledge that will help me in my future career.
           </p>
           <p>
             You can find my skills, projects, and links below. Feel free to
